d6b91456daa4af32f6f630fb38dba3a090947664
Алексей <setgecko@gmail.com>
2023-03-23 19:47:07 +0300

    Скринкаст по интерфейсам 10.1

---
+++ Screencast_10.1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Screencast_10._1
+{
+    internal class Program
+    {
+        static ILogger Logger { get; set; }
+        static void Main(string[] args)
+        {
+            Logger = new Logger();
+
+            var worker1 = new Worker1(Logger);
+            var worker2 = new Worker2(Logger);
+            var worker3 = new Worker3(Logger);
+
+            worker1.Work();
+            worker2.Work();
+            worker3.Work();
+        }
+    }
+    public interface ILogger
+    {
+        void Event(string message);
+        void Error(string message);
+    }
+    public class Logger : ILogger
+    {
+        public void Error(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Event(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+    public interface IWorker
+    {
+        void Work();
+    }
+}
