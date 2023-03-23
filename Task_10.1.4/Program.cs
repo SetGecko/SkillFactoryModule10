1695d8490cf9273e8017f133558d3c1e3a9cd978
Алексей <setgecko@gmail.com>
2023-03-23 19:49:27 +0300

    Выполнил задание 10.1.4

---
+++ Task_10.1.4/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Task_10._1._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    public interface IManager
+    {
+        void Create();
+        void Read();
+        void Update();
+        void Delete();
+    }
+    public class Manager : IManager
+    {
+        public void Create()
+        {
+
+        }
+
+        public void Read()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Delete()
+        {
+
+        }
+    }
+}
