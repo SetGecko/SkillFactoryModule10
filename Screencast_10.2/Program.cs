2455c87565d8e1ff3b595855d64a03c6d2b1c032
Алексей <setgecko@gmail.com>
2023-03-24 20:28:10 +0300

    Практика по скринкасту 10.2 явная и неявная реализация интерфейса

---
+++ Screencast_10.2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Screencast_10._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    public interface ICalculator
+    {
+        void Solve(int number);
+        void Solve(int numberOne, int numberTwo);
+    }
+    public class BaseCalculator : ICalculator
+    {
+        void ICalculator.Solve(int number)
+        {
+            
+        }
+
+        void ICalculator.Solve(int numberOne, int numberTwo)
+        {
+            
+        }
+    }
+}
