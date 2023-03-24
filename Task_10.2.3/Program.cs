8bbd7bb5e52d53d057cf65b3758f62cc99b1ac50
Алексей <setgecko@gmail.com>
2023-03-24 20:33:13 +0300

    Выполнил задание 10.2.3

---
+++ Task_10.2.3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_10._2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    public interface IWorker
+    {
+        public void Build();
+    }
+    public class Worker : IWorker
+    {
+        public void Build()
+        {
+
+        }
+    }
+}
