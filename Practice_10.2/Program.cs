101d72cb7788d03172c250849a17f96175b9f8e7
Алексей <setgecko@gmail.com>
2023-03-24 20:18:29 +0300

    Практика 10.2 - явная реализация интерфейса

---
+++ Practice_10.2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Practice_10._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NewMessenger newMessenger = new NewMessenger();
+
+            ((IWhatsApp)newMessenger).SendMessage("Hello World!");
+            ((IViber)newMessenger).SendMessage("Hello World!");
+        }
+    }
+    public interface IWhatsApp
+    {
+        public void SendMessage(string message);
+    }
+    public interface IViber
+    {
+        public void SendMessage(string message);
+    }
+    public class NewMessenger : IWhatsApp, IViber
+    {
+        void IWhatsApp.SendMessage(string message)
+        {
+            Console.WriteLine("{0} : {1}", "WhatsApp", message);
+        }
+        void IViber.SendMessage(string message)
+        {
+            Console.WriteLine("{0} : {1}", "IViber", message);
+        }
+    }
+}
