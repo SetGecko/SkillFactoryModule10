1c288882c0c4b2ca62cfd7fce48bf6c7a4839b3e
Алексей <setgecko@gmail.com>
2023-03-24 20:35:25 +0300

    Выполнил задание 10.2.4

---
+++ Task_10.2.4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task_10._2._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker worker = new Worker();
+            ((IWorker)worker).Build();
+        }
+    }
+    public interface IWorker
+    {
+        public void Build();
+    }
+    public class Worker : IWorker
+    {
+        void IWorker.Build()
+        {
+            Console.WriteLine("Явно реализованный интерфейс IWorker");
+        }
+    }
+}
