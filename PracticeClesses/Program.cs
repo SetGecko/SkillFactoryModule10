009f4267250f4cb15e01c0d9692357e6ea559cd1
Алексей <setgecko@gmail.com>
2023-03-24 20:09:02 +0300

    Немного практики с классами

---
+++ PracticeClesses/Program.cs
@@ -0,0 +1,15 @@
+﻿using PracticeClasses;
+
+namespace PracticeClesses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person set = new Person();
+            set.name = "Test";
+            set.age = 1;
+            set.Print();
+        }
+    }
+}
