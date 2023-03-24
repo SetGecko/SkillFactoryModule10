4e1d4dd2caaddcdaf20b70bd0f68d912e2a6d9c8
Алексей <setgecko@gmail.com>
2023-03-24 20:31:43 +0300

    Выполнил задание 10.2.2

---
+++ Task_10.2.2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task_10._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Writer writer = new Writer();
+            ((IWriter)writer).Write();
+        }
+    }
+    public interface IWriter
+    {
+        void Write();
+    }
+    public class Writer : IWriter
+    {
+        void IWriter.Write()
+        {
+            Console.WriteLine("Я пример явной реализации интерфейса");
+        }
+    }
+}
