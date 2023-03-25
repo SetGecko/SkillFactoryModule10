5223f4bc6bb7e59089e18c5051dd3ed1e83f4849
Алексей <setgecko@gmail.com>
2023-03-25 09:52:33 +0300

    Скринкаст множественная реализация интерфейсов

---
+++ Practice_10.3/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Practice_10._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    public interface IWriter
+    {
+        void Write();
+    }
+    public interface IReader
+    {
+        void Read();
+    }
+    public interface IMailer
+    {
+        void SendMail();
+    }
+    class FileManager : IWriter, IReader, IMailer
+    {
+        public void Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendMail()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Screencast_10.3/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Screencast_10._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IFile file = new FileInfo();
+            IBinaryFile binaryFile = new FileInfo();
+            FileInfo fileInfo = new FileInfo();
+
+            file.ReadFile();
+
+            binaryFile.ReadFile();
+            binaryFile.OPenBinaryFile();
+
+            fileInfo.Search("Строка для поиска");
+
+        }
+    }
+    public interface IFile
+    {
+        void ReadFile();
+    }
+    public interface IBinaryFile
+    {
+        void ReadFile();
+        void OPenBinaryFile();
+    }
+    class FileInfo : IFile, IBinaryFile
+    {
+        void IBinaryFile.OPenBinaryFile()
+        {
+            Console.WriteLine("Открываем бинарный файл");
+        }
+
+        void IFile. ReadFile()
+        {
+            Console.WriteLine("Читаем текстовыый файл");
+        }
+        void IBinaryFile.ReadFile()
+        {
+            Console.WriteLine("Читаем бинарный файл");
+        }
+        public void Search(string text)
+        {
+            Console.WriteLine("Начался поиск текста в файле");
+        }
+    }
+}
