40fa02aaea89076b94b41f0169b4ea28ce8ab3d1
Алексей <setgecko@gmail.com>
2023-03-25 10:05:13 +0300

    Выполнил задание 10.3.3

---
+++ Task_10.3.3/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Task_10._3._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    public interface IBook
+    {
+        void Read();
+    }
+
+    public interface IDevice
+    {
+        void TurnOn();
+        void TurnOff();
+    }
+    class ElectronicBook : IBook, IDevice
+    {
+        void IDevice.TurnOff()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDevice.TurnOn()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IBook.Read()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
