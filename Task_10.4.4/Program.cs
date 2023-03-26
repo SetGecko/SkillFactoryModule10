1095422d1639597f2e2ae858cdaad49ba490f7f2
Алексей <setgecko@gmail.com>
2023-03-26 16:20:32 +0300

    Выполнил задание 10.4.4

---
+++ Task_10.4.4/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Task_10._4._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User();
+            var account = new Account();
+
+            IUpdater<Account> updater = new UserService();
+
+            var userService = new UserService();
+            userService.Update(user);
+        }
+    }
+    public interface IUpdater<inT>
+    {
+        void Update(T entity);
+    }
+
+    public class UserService : IUpdater<User>
+    {
+        public void Update(User entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class User
+    {
+
+    }
+
+    public class Account : User
+    {
+
+    }
+}
