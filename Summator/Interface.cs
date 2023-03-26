df06f09e9cb877cc9b37535a3da5848071cbb8b0
Алексей <setgecko@gmail.com>
2023-03-26 16:25:16 +0300

    Выполнил итоговое задание модуля 10

---
+++ Summator/Interface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summator
+{
+    public interface ISum
+    {
+        int Sum(int a, int b);
+    }
+
+    public interface ILogger
+    {
+        void EventLog(string message);
+
+        void ErrorLog(string message);
+    }
+}
---
+++ Summator/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Summator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите первое число: ");
+                var int1 = int.Parse(Console.ReadLine());
+                Console.Write("Введите второе число: ");
+                var int2 = int.Parse(Console.ReadLine());
+
+                var summator = new Summator(new Logger());
+
+                Console.WriteLine($"Сумма: {summator.Sum(int1, int2)}");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Summator : ISum
+    {
+        private ILogger logger;
+
+        public Summator(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public int Sum(int a, int b)
+        {
+            try
+            {
+                logger.EventLog($"Сложение чисел {a} и {b}");
+                return checked(a + b);
+            }
+            catch (Exception exception)
+            {
+                logger.ErrorLog(exception.Message);
+                return 0;
+            }
+        }
+    }
+
+    public class Logger : ILogger
+    {
+        public void ErrorLog(string message)
+        {
+            var defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = defaultColor;
+        }
+
+        public void EventLog(string message)
+        {
+            var defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(message);
+            Console.ForegroundColor = defaultColor;
+        }
+    }
+}
